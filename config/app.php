ae4e22931844a63c3d3fc2fecf7446df03206057
Ivan Zakharchenko <ipzakharchenko@gmail.com>
2023-12-13 12:36:45 +0200

    add redirect to dashboard

--- config/app.php
+++ config/app.php
@@ -185,4 +185,8 @@ return [
         // 'Example' => App\Facades\Example::class,
     ])->toArray(),
 
+    /**
+     * Our global variables
+     */
+    'blogName' => env('APP_BLOG_NAME', 'Laravel'),
 ];
--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::prefix('dashboard')->group(function () {
+    Route::get('/home', [App\Http\Controllers\Dashboard\DashboardController::class, 'index'])->name('dashboard.home');
+});
