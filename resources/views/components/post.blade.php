a0d4ebfc85335176f1fb2c547bbbb16981408a05
Ivan Zakharchenko <ipzakharchenko@gmail.com>
2023-12-26 16:51:42 +0200

    add format date to post

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -1,7 +1,6 @@
 <article class="blog-post" {{ $post }}>
     <h2 class="display-5 link-body-emphasis mb-1"><a href="#" class="blog-post-link"></a>{{ $post->title }}</h2>
-    <p class="blog-post-meta">January 1, 2021 by {{$post->user->full_name}}</p>
-
+    <p class="blog-post-meta">{{ $post->created_at->format('M, d Y')  }} by {{ $post->user->full_name }}</p>
     <p>{{ Str::of($post->text)->limit(220) }}</p>
     <hr>
 </article>
