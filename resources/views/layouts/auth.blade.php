bed72198c713196af12973e5fcb3033dad343cae
Dmytro Trotsenko <3339bender@gmail.com>
2023-12-31 13:57:13 +0200

    removeUselessLink

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Auth</title>
+    <title>Blog about web design Auth</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
