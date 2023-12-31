3db5d2656506bf86fcec8baf7e02e39718b109e0
Dmytro Trotsenko <3339bender@gmail.com>
2023-12-31 02:09:53 +0200

    AddTitlesForPages

--- resources/views/dashboard/layouts/app.blade.php
+++ resources/views/dashboard/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Dashboard</title>
+    <title>Blog about web design Dashboard</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Blog about web design</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
